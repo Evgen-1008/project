2f103476bf0ba08a7579237d5ff763c6a904e24d
Evgeniy <evgeniy.sakriev@mail.ru>
2025-09-26 21:35:15 +0500

    тесты для функций get_mask_number

--- tests/test_get_mask_card_number.py
+++ tests/test_get_mask_card_number.py
@@ -1,10 +1,9 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_card_number
 
 
 def test_get_mask_cart_number():
     assert get_mask_card_number("7556 7748 8893 8885") == "7556 77** **** 8885"
 
-def test_get_mask_account():
-    assert get_mask_account("4544565336675446") == "**5446"
+
 
 
